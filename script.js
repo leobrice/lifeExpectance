7d22a3c531302b4256106af1508538dc691b24d7
leobrice <100925957+leobrice@users.noreply.github.com>
2022-12-22 13:38:42 +0000

    Adding
    Adding Startup files

---
+++ script.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", function(){
+// // document.querySelector("button").click= function(){
+//     // var sleep=document.getElementById("sleeping1").value;
+//     // alert("mambo "+sleep);
+// // }
+// // def function myFunction(){};
+// document.querySelector("button").addEventListener("click",myFunction);
+// function myFunction(){
+//     // var g=document.getElementById("sleeping1").value;
+//     alert(g);
+// }
+
+// })
